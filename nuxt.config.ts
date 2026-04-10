ef1ea3bc4784f9e0bf85877600f9f0637f6baa63
Florian <112929561+guerrinflorian@users.noreply.github.com>
2026-04-10 06:40:39 +0000

    feat: ajout favicons complets et site.webmanifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -60,6 +60,10 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
         { rel: 'canonical', href: 'https://florian-guerrin.fr' },
       ],
       htmlAttrs: { lang: 'fr' },
